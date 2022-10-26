71d7da4ea8ca88dbf3e30b492e45bbdcfa402732
Hoshhh <nyxious123@live.com>
2022-10-26 03:43:32 -0400

    Added the ability to pull the JSON data from the api into the frontend

--- components/MediaCard.js
+++ components/MediaCard.js
@@ -2,11 +2,10 @@
 
 const MediaCard = () => {
   return (
-    <div className="container flex flex-col max-w-md max-h-md min-h-max bg-black items-center p-6 m-2 rounded-md">
-        <h1 className="text-lg">TITLE HERE going to test an absurdly large title so i know how bad this is gonna look</h1>
+    <div className="container flex flex-col max-w-md max-h-md min-h-max bg-purple items-center p-6 m-2 rounded-md">
         <iframe width="400" height="260" src="https://www.youtube.com/embed/EbxkOntz2SA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <h1 className="text-xl pb-6 font-bold text-white">TITLE HERE going to test an absurdly large title so i know how bad this is gonna look</h1>
         <h2 className="text-md">Description oh yea, i forgot that multiple symbols dont get wrapped for some reason, i still have no idea why thats the case but any ways heres the test</h2>
-        <span>***** rating</span>
     </div>
   )
 }
--- pages/videos.js
+++ pages/videos.js
@@ -1,10 +1,26 @@
 import Link from "next/link"
+import { useEffect, useState } from "react"
 import MediaCard from "../components/MediaCard"
 import Navbar from "../components/Navbar"
+
 export default function Home() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+            const response = await fetch("/api/videos");
+            const res = await response.json();
+
+            setData(res);
+        }
+        fetchVideos();
+    }, [])
+
+    console.log(data);
+
   return (
     <div className="h-full w-screen bg-gradient-to-t from-purple to-black min-w-fit" >
-        <div className="container mx-auto flex flex-col h-screen bg-slate-500">
+        <div className="container mx-auto flex flex-col h-full">
             <Navbar />
             <div className="container mx-auto text-white flex flex-row flex-wrap justify-center items-center h-full">
                 <MediaCard />
